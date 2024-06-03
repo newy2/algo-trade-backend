624c3410232c5f06193b5ad89b71f3405294f29d
Jonghoon Yoon <by.newy@gmail.com>
2024-06-04 01:08:05 +0900

    refector: addCandles 파라미터 타입 변경

--- api-server/core/coroutine-based-application/src/main/kotlin/com/newy/algotrade/coroutine_based_application/price2/application/service/candle/AddCandlesService.kt
+++ api-server/core/coroutine-based-application/src/main/kotlin/com/newy/algotrade/coroutine_based_application/price2/application/service/candle/AddCandlesService.kt
@@ -2,14 +2,14 @@ package com.newy.algotrade.coroutine_based_application.price2.application.servic
 
 import com.newy.algotrade.coroutine_based_application.price2.port.`in`.candle.AddCandlesUseCase
 import com.newy.algotrade.coroutine_based_application.price2.port.out.AddCandlePort
-import com.newy.algotrade.domain.chart.Candle
 import com.newy.algotrade.domain.chart.Candles
+import com.newy.algotrade.domain.common.extension.ProductPrice
 import com.newy.algotrade.domain.price.domain.model.ProductPriceKey
 
 class AddCandlesService(
     private val candlePort: AddCandlePort
 ) : AddCandlesUseCase {
-    override fun addCandles(productPriceKey: ProductPriceKey, candleList: List<Candle>): Candles {
+    override fun addCandles(productPriceKey: ProductPriceKey, candleList: List<ProductPrice>): Candles {
         return candlePort.addCandles(productPriceKey, candleList)
     }
 }
--- api-server/core/coroutine-based-application/src/main/kotlin/com/newy/algotrade/coroutine_based_application/price2/port/in/candle/AddCandlesUseCase.kt
+++ api-server/core/coroutine-based-application/src/main/kotlin/com/newy/algotrade/coroutine_based_application/price2/port/in/candle/AddCandlesUseCase.kt
@@ -1,9 +1,9 @@
 package com.newy.algotrade.coroutine_based_application.price2.port.`in`.candle
 
-import com.newy.algotrade.domain.chart.Candle
 import com.newy.algotrade.domain.chart.Candles
+import com.newy.algotrade.domain.common.extension.ProductPrice
 import com.newy.algotrade.domain.price.domain.model.ProductPriceKey
 
 interface AddCandlesUseCase {
-    fun addCandles(productPriceKey: ProductPriceKey, candleList: List<Candle>): Candles
+    fun addCandles(productPriceKey: ProductPriceKey, candleList: List<ProductPrice>): Candles
 }
