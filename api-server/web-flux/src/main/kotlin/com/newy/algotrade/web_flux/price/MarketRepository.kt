51e0676b02691e8d750df160099714fc95e909f0
Jonghoon Yoon <by.newy@gmail.com>
2024-07-15 20:52:54 +0900

    feat: 미사용 코드 제거

--- api-server/web-flux/src/main/kotlin/com/newy/algotrade/web_flux/price/MarketRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.newy.algotrade.web_flux.price
-
-import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface MarketRepository : CoroutineCrudRepository<Market, Long>
