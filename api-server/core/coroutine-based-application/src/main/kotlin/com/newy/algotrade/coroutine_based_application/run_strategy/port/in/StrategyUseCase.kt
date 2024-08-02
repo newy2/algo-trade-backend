7747efb627e2cd23ef5fa9e33ff3a07b95c6f784
Jonghoon Yoon <by.newy@gmail.com>
2024-08-02 19:08:17 +0900

    refector: 미사용 코드 제거

--- api-server/core/coroutine-based-application/src/main/kotlin/com/newy/algotrade/coroutine_based_application/run_strategy/port/in/StrategyUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.newy.algotrade.coroutine_based_application.run_strategy.port.`in`
-
-import com.newy.algotrade.coroutine_based_application.run_strategy.port.`in`.model.UserStrategyKey
-
-interface StrategyUseCase {
-    fun setStrategy(key: UserStrategyKey)
-    fun removeStrategy(key: UserStrategyKey)
-}
