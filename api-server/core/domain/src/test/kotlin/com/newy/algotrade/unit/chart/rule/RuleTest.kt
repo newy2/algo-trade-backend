93c595df2d5d6e6362e1b74908b23fd791ce0043
Jonghoon Yoon <by.newy@gmail.com>
2024-12-21 21:41:23 +0900

    #17 fix: 테스트 에러 수정

--- api-server/core/domain/src/test/kotlin/com/newy/algotrade/unit/chart/rule/RuleTest.kt
+++ api-server/core/domain/src/test/kotlin/com/newy/algotrade/unit/chart/rule/RuleTest.kt
@@ -2,7 +2,6 @@ package com.newy.algotrade.unit.chart.rule
 
 import com.newy.algotrade.chart.domain.indicator.ConstDecimalIndicator
 import com.newy.algotrade.chart.domain.rule.*
-import com.newy.algotrade.chart.rule.*
 import helpers.BooleanArrayRule
 import helpers.BooleanRule
 import helpers.FixedDecimalIndicator
