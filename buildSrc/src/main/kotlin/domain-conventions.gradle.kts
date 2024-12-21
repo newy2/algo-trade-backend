de47425b66d086ccd705528b486198686034099a
Jonghoon Yoon <by.newy@gmail.com>
2024-12-22 02:00:20 +0900

    #17 fix: 런타임 에러 수정

--- buildSrc/src/main/kotlin/domain-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/domain-conventions.gradle.kts
@@ -7,8 +7,7 @@ dependencies {
     implementation("jakarta.validation:jakarta.validation-api:3.0.2") // web-flux 모듈에서 사용
     implementation("org.hibernate.validator:hibernate-validator:8.0.1.Final")
     implementation("org.glassfish.expressly:expressly:5.0.0")
-
-    // TODO web-flux, ktor 모두 jackson 을 사용한다면, api 로 변경할 것
-    compileOnly("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
+    
     testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
 }
