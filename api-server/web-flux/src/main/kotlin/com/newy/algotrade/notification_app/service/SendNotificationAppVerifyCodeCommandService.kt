3b1f66d24c9b48b05f88becd46937dfb856b162a
Jonghoon Yoon <by.newy@gmail.com>
2025-01-22 19:28:45 +0900

    #3 refector: 알림앱 인증코드 발급 로직 리팩토링

--- api-server/web-flux/src/main/kotlin/com/newy/algotrade/notification_app/service/SendNotificationAppVerifyCodeCommandService.kt
+++ api-server/web-flux/src/main/kotlin/com/newy/algotrade/notification_app/service/SendNotificationAppVerifyCodeCommandService.kt
@@ -2,7 +2,6 @@ package com.newy.algotrade.notification_app.service
 
 import com.newy.algotrade.common.event.SendNotificationMessageEvent
 import com.newy.algotrade.notification_app.domain.NotificationApp
-import com.newy.algotrade.notification_app.domain.Webhook
 import com.newy.algotrade.notification_app.port.`in`.SendNotificationAppVerifyCodeInPort
 import com.newy.algotrade.notification_app.port.`in`.model.SendNotificationAppVerifyCodeCommand
 import com.newy.algotrade.notification_app.port.out.FindNotificationAppOutPort
@@ -13,22 +12,16 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Transactional
 class SendNotificationAppVerifyCodeCommandService(
     private val findNotificationAppOutPort: FindNotificationAppOutPort,
     private val saveNotificationAppOutPort: SaveNotificationAppOutPort,
     private val sendNotificationMessageOutPort: SendNotificationMessageOutPort,
 ) : SendNotificationAppVerifyCodeInPort {
+    @Transactional
     override suspend fun sendVerifyCode(command: SendNotificationAppVerifyCodeCommand): String {
-        val notificationApp = findNotificationAppOutPort.findByUserId(command.userId) ?: NotificationApp(
-            userId = command.userId,
-            webhook = Webhook(
-                type = command.webhookType,
-                url = command.webhookUrl
-            )
-        )
-
-        val newNotificationApp = notificationApp.validate(command.webhookUrl).generateVerifyCode()
+        val newNotificationApp = findNotificationApp(command)
+            .validate(command.webhookUrl)
+            .generateVerifyCode()
 
         saveNotificationAppOutPort.save(newNotificationApp)
         useTransactionHook(
@@ -44,4 +37,7 @@ class SendNotificationAppVerifyCodeCommandService(
 
         return newNotificationApp.verifyCode
     }
+
+    private suspend fun findNotificationApp(command: SendNotificationAppVerifyCodeCommand): NotificationApp =
+        findNotificationAppOutPort.findByUserId(command.userId) ?: command.toDomainModel()
 }
