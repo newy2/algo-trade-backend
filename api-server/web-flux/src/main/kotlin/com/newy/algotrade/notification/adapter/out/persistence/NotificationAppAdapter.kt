59fbfa6b64fd4af4fe2e96fbe7f9216b8f437289
Jonghoon Yoon <by.newy@gmail.com>
2025-01-25 22:51:54 +0900

    #3 feat: NotificationAppAdapter 구현

---
+++ api-server/web-flux/src/main/kotlin/com/newy/algotrade/notification/adapter/out/persistence/NotificationAppAdapter.kt
@@ -0,0 +1,37 @@
+package com.newy.algotrade.notification.adapter.out.persistence
+
+import com.newy.algotrade.notification.adapter.out.persistence.repository.UserNotificationAppR2dbcRepository
+import com.newy.algotrade.notification.adapter.out.persistence.repository.UserNotificationAppVerifyCodeR2dbcRepository
+import com.newy.algotrade.notification.domain.NotificationApp
+import com.newy.algotrade.notification.domain.Webhook
+import com.newy.algotrade.notification.port.out.FindNotificationAppOutPort
+import com.newy.algotrade.spring.annotation.PersistenceAdapter
+import org.springframework.beans.factory.annotation.Autowired
+
+@PersistenceAdapter("NotificationAppAdapterForNotificationPackage")
+class NotificationAppAdapter(
+    @Autowired private val notificationAppRepository: UserNotificationAppR2dbcRepository,
+    @Autowired private val notificationVerifyCodeRepository: UserNotificationAppVerifyCodeR2dbcRepository,
+) : FindNotificationAppOutPort {
+    override suspend fun findNotificationApp(
+        userId: Long,
+        isVerified: Boolean
+    ): NotificationApp? =
+        notificationAppRepository.findByUserIdAndUseYn(
+            userId = userId,
+            useYn = "Y"
+        )?.let { notificationApp ->
+            notificationVerifyCodeRepository.findByUserNotificationAppIdAndVerifyYn(
+                userNotificationAppId = notificationApp.id,
+                verifyYn = if (isVerified) "Y" else "N"
+            )?.let {
+                NotificationApp(
+                    id = notificationApp.id,
+                    webhook = Webhook.from(
+                        type = notificationApp.type,
+                        url = notificationApp.url,
+                    )
+                )
+            }
+        }
+}
--- api-server/web-flux/src/main/kotlin/com/newy/algotrade/spring/annotation/ExternalSystemAdapter.kt
+++ api-server/web-flux/src/main/kotlin/com/newy/algotrade/spring/annotation/ExternalSystemAdapter.kt
@@ -1,8 +1,12 @@
 package com.newy.algotrade.spring.annotation
 
+import org.springframework.core.annotation.AliasFor
 import org.springframework.stereotype.Component
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 @Component
-annotation class ExternalSystemAdapter
+annotation class ExternalSystemAdapter(
+    @get:AliasFor(annotation = Component::class, attribute = "value")
+    val value: String = ""
+)
