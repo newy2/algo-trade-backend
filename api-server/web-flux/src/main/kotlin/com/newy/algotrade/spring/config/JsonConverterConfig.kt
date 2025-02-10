9e1e2ebf43eb904562b695073d9cf99ae2d02879
Jonghoon Yoon <by.newy@gmail.com>
2025-02-10 17:57:02 +0900

    fix: LocalDateTime 을 JSON 으로 변환 시, 날짜 포맷이 변환되지 않는 현상 수정

--- api-server/web-flux/src/main/kotlin/com/newy/algotrade/spring/config/JsonConverterConfig.kt
+++ api-server/web-flux/src/main/kotlin/com/newy/algotrade/spring/config/JsonConverterConfig.kt
@@ -1,6 +1,8 @@
 package com.newy.algotrade.spring.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.newy.algotrade.common.mapper.JsonConverter
 import com.newy.algotrade.common.mapper.JsonConverterByJackson
@@ -10,7 +12,10 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class JsonConverterConfig {
     @Bean
-    fun objectMapper(): ObjectMapper = jacksonObjectMapper()
+    fun objectMapper(): ObjectMapper =
+        jacksonObjectMapper()
+            .registerModule(JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
 
     @Bean
     fun jsonConverter(objectMapper: ObjectMapper): JsonConverter = JsonConverterByJackson(objectMapper)
